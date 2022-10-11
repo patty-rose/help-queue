171fac1e532224c750e8e7d5e892d6730b09e985
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-10 21:02:33 -0700

    update Ticket and TicketList components

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Ticket(){
+function Ticket(props){//props as an argument gives us access to the props in the parent component: 'TicketList'. You can see the props being instantiated in the React.Fragment JSX -- {props.lovation} - {props.names} etc.
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
