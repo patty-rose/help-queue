b01cc9000d8bc9a4066b82c297643c7ed08563a9
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-23 15:18:27 -0700

    add tests and reducers for ticketList

---
+++ src/_tests_/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,67 @@
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+describe('ticketListReducer', () => {
+
+  let action;//1.We declare an action but don't define it yet. Each of our new tests will define what the action should be (whether that is adding, updating, or deleting a ticket).
+  const ticketData = {//2.We create a ticketData constant that provides ticket information for testing purposes.
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  };
+
+  const currentState = { //for Delete test
+    1: {
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1 
+    }, 2: {
+      names: 'Jasmine and Justine',
+      location: '2a',
+      issue: 'Reducer has side effects.',
+      id: 2 
+    }
+  }
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+    // our reducer will take two arguments. The first argument is the current state while the second argument is an action that will be applied to the current state. Note that the action's type is stored inside an object. This object can potentially contain other things besides the name of the action itself. We will cover this further in the next lesson.
+  });
+
+  test('Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData;//use ES6 destructuring syntax to provide keys from our ticketData
+    action = {//our reducer takes an object as an argument instead of just a string for the action type itself. Because it takes an object, it can take multiple key-value pairs that include additional information about the action the reducer will need to take.
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {
+        names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 
+      }
+    });
+  });
+
+});
---
+++ src/reducers/ticket-list-reducer.js
@@ -0,0 +1,29 @@
+//Ultimately, a reducer is just a pure function that contains a conditional. What the reducer does is dependent on the action passed in as an argument.
+
+
+const reducer = (state = {}, action) => {//
+  const { names, location, issue, id } = action;
+  switch (action.type) {//our switch will be based on the action.type. Because the action parameter takes an object, need to look at the action's type
+  case 'ADD_TICKET':
+    return Object.assign({}, state, {//use Object.assign() to clone the state object. for this to work correctly, Object.assign() must take three arguments: 1. {} empty object as first object means it will clone the object isntead of mutating it. 2. second argument is object that will be cloned-- current state. 3. 3rd argument is the change that will be made to our new copy. 
+      [id]: {//creates new key-value pari in state obj. with id as key and value as ticket obj.
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+    case 'DELETE_TICKET':
+    let newState = { ...state };//make a copy of state
+    delete newState[id];//delete tic from copy
+    return newState;
+  default:
+    return state; //Our reducer hasn't altered anything. Instead, it made a copy of the state that was passed in as argument, altered the copy, and then returned the altered copy so it can be used elsewhere in our code.
+  }
+
+};
+//our function has two parameters. The first is the state(state = {}) that will need to be changed while the second is the action that will be applied to that state.
+
+
+
+export default reducer; //Note that our export statement includes default because this file will only have one function inside it — the reducer for our ticket list. That way, when we import the reducer into our test or anywhere else, we can store all the code directly inside a single variable such as ticketListReducer.
